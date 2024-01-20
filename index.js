c72efb90f25663fdc8c2bf0bf6dffe5769da7cbb
CHATELAIN JORDAN <jordan139@hotmail.fr>
2024-01-20 15:07:19 +0100

    modif code js

--- index.js
+++ index.js
@@ -1,70 +1,55 @@
-var count = 0;
-var timerInterval;
+var initialCount = 0; // Ajout de la variable pour stocker la valeur initiale du minuteur
+var count = 0; // Initialiser à 0 secondes
+var timerTimeout;
 var isTimerRunning = false;
 var display = document.querySelector("#time");
 var audioElement = new Audio("assets/bang.wav");
+var imgBombe = document.querySelector(".img_bombe");
 
 function startTimer(duration) {
-  var timer = duration + count,
-    minutes,
-    seconds;
+  var minutes, seconds;
 
-  timerInterval = setInterval(function () {
-    minutes = parseInt(timer / 60, 10);
-    seconds = parseInt(timer % 60, 10);
+  function update() {
+    minutes = parseInt(duration / 60, 10);
+    seconds = parseInt(duration % 60, 10);
 
     minutes = minutes < 10 ? "0" + minutes : minutes;
     seconds = seconds < 10 ? "0" + seconds : seconds;
 
     display.textContent = minutes + ":" + seconds;
 
-    if (--timer < 0) {
+    if (duration === 0) {
       handleTimerExpiration();
+    } else {
+      timerTimeout = setTimeout(function () {
+        update();
+      }, 1000);
     }
-  }, 1000);
+
+    duration--;
+  }
+
+  update();
 }
 
 function handleTimerExpiration() {
-  var imgBombe = document.querySelector(".img_bombe");
   imgBombe.src = "assets/boum.png";
+  audioElement.play().then(function () {
+    audioElement.addEventListener("ended", function () {
+      resetTimer();
+    });
+  });
 
-  // Jouer le son
-  audioElement.play();
-
-  clearInterval(timerInterval);
   isTimerRunning = false;
 }
 
 function resetTimer() {
-  count = 0;
+  count = initialCount; // Réinitialiser à la valeur initiale
   isTimerRunning = false;
   updateTimerDisplay();
-  var imgBombe = document.querySelector(".img_bombe");
   imgBombe.src = "assets/bombe.png";
-  clearInterval(timerInterval);
-  timerInterval = null;
-}
-
-function addTimeToTimer(seconds) {
-  if (!isTimerRunning) {
-    count += seconds;
-    updateTimerDisplay();
-  } else {
-    count += seconds;
-    clearInterval(timerInterval);
-    startTimer(count);
-  }
-}
-
-function subtractTimeFromTimer(seconds) {
-  if (!isTimerRunning && count >= seconds) {
-    count -= seconds;
-    updateTimerDisplay();
-  } else if (isTimerRunning && count >= seconds) {
-    count -= seconds;
-    clearInterval(timerInterval);
-    startTimer(count);
-  }
+  clearTimeout(timerTimeout);
+  timerTimeout = null;
 }
 
 function updateTimerDisplay() {
@@ -78,31 +63,38 @@ function updateTimerDisplay() {
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  var goButton = document.querySelector(".go");
+  var plusButton = document.querySelector(".bouton_plus");
+  var moinsButton = document.querySelector(".bouton_moins");
+  var suivantButton = document.querySelector(".bouton_suivant");
+  var goButton = document.querySelector(".bouton_go");
   var resetButton = document.querySelector("#resetTimer");
 
-  goButton.addEventListener("click", function () {
+  plusButton.addEventListener("click", function () {
     if (!isTimerRunning) {
-      resetTimer();
-      startTimer(10); // Démarrer le minuteur avec la durée souhaitée (10 secondes dans cet exemple)
-      isTimerRunning = true;
+      count += 30;
+      initialCount = count; // Mettre à jour la valeur initiale lors de l'ajout de temps
+      updateTimerDisplay();
     }
-    addTimeToTimer(30);
   });
 
-  resetButton.addEventListener("click", function () {
+  moinsButton.addEventListener("click", function () {
+    if (!isTimerRunning && count >= 30) {
+      count -= 30;
+      initialCount = count; // Mettre à jour la valeur initiale lors de la soustraction de temps
+      updateTimerDisplay();
+    }
+  });
+
+  suivantButton.addEventListener("click", function () {
     resetTimer();
   });
 
-  var addButton = document.querySelector(".bouton_plus");
-  var subtractButton = document.querySelector(".bouton_moins");
+  updateTimerDisplay();
 
-  addButton.addEventListener("click", function () {
-    addTimeToTimer(30);
+  goButton.addEventListener("click", function () {
+    if (!isTimerRunning) {
+      startTimer(count);
+      isTimerRunning = true;
+    }
   });
-
-  subtractButton.addEventListener("click", function () {
-    subtractTimeFromTimer(30);
-  });
-  resetTimer();
 });
